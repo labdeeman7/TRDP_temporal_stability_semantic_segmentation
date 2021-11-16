e2f3f4443d778e0a6112fa05b65bde8f51be1785
Oluwatosin Alabi <labdeeman77@gmail.com>
2021-11-16 17:41:38 +0100

    more changes for colab

--- configs/tsmnet/tsmnet_r50-d1_769x769_40k_cityscapes_video.py
+++ configs/tsmnet/tsmnet_r50-d1_769x769_40k_cityscapes_video.py
@@ -1,4 +1,4 @@
 _base_ = [
     '../_base_/models/tsm_r50-d8.py', '../_base_/datasets/cityscapes_769x769.py',
-    '../_base_/default_runtime.py', '../_base_/schedules/schedule_20k.py'
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_40k.py'
 ]
