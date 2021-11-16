1d88107cb4ec1237fb6551133c244b14dfda2758
Oluwatosin Alabi <labdeeman77@gmail.com>
2021-11-16 04:06:20 +0100

    _init errors

--- configs/tsmnet/tsmnet_r50-d1_769x769_20k_cityscapes_video.py
+++ configs/tsmnet/tsmnet_r50-d1_769x769_20k_cityscapes_video.py
@@ -1,13 +1,4 @@
 _base_ = [
-    '../../_base_/models/tsm_r50-d8.py', '../../_base_/datasets/cityscapes_769x769.py',
-    '../../_base_/default_runtime.py', '../../_base_/schedules/schedule_20k.py'
+    '../_base_/models/tsm_r50-d8.py', '../_base_/datasets/cityscapes_769x769.py',
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_20k.py'
 ]
-
-model = dict(
-    pretrained='torchvision://resnet18',
-    backbone=dict(type='ResNet', depth=18),
-    decode_head=dict(
-        in_channels=512,
-        channels=128,
-    ),
-    auxiliary_head=dict(in_channels=256, channels=64))
--- mmseg/models/segmentors/__init__.py
+++ mmseg/models/segmentors/__init__.py
@@ -2,5 +2,6 @@
 from .base import BaseSegmentor
 from .cascade_encoder_decoder import CascadeEncoderDecoder
 from .encoder_decoder import EncoderDecoder
+from .tsm_encoder_decoder import TSMEncoderDecoder
 
 __all__ = ['BaseSegmentor', 'EncoderDecoder', 'CascadeEncoderDecoder', 'TSMEncoderDecoder'] 
